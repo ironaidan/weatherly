4a1c21f03a4333181179a06f216a94aeab7028d4
iron_aidan <39450202+ironaidan@users.noreply.github.com>
2024-08-31 09:15:20 -0700

    Fix app test

--- app/src/androidTest/kotlin/com/aidannemeth/weatherly/WeatherlyAppTest.kt
+++ app/src/androidTest/kotlin/com/aidannemeth/weatherly/WeatherlyAppTest.kt
@@ -1,28 +1,20 @@
 package com.aidannemeth.weatherly
 
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.platform.app.InstrumentationRegistry
-import com.aidannemeth.weatherly.navigation.Home
-import com.aidannemeth.weatherly.ui.theme.WeatherlyTheme
 import org.junit.Rule
 import org.junit.Test
 
 class WeatherlyAppTest {
 
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Test
     fun `title is displayed when app is launched`() {
         val context = InstrumentationRegistry.getInstrumentation().targetContext
-
-        composeTestRule.setContent {
-            WeatherlyTheme {
-                Home()
-            }
-        }
 
         composeTestRule
             .onNodeWithText(context.getString(R.string.app_name))
