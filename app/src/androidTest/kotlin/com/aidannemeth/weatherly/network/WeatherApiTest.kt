31e4f436b73cfdb0c730f919869beb3517c678f0
iron_aidan <39450202+ironaidan@users.noreply.github.com>
2025-01-29 14:14:58 -0800

    Refactor WeatherApiTest and remove NavigationTest
    - Refactored WeatherApiTest to use more descriptive test names and improve readability.
    - Replaced `assertThrows` with `assertFailsWith` for more idiomatic Kotlin error handling.
    - Removed `NavigationTest` file.

--- app/src/androidTest/kotlin/com/aidannemeth/weatherly/network/WeatherApiTest.kt
+++ app/src/androidTest/kotlin/com/aidannemeth/weatherly/network/WeatherApiTest.kt
@@ -7,14 +7,12 @@ import com.aidannemeth.weatherly.feature.weather.domain.model.Longitude
 import com.aidannemeth.weatherly.feature.weather.domain.model.Units
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import okhttp3.mockwebserver.SocketPolicy
 import okio.Buffer
 import okio.ProtocolException
-import org.junit.Assert.assertThrows
 import org.junit.Rule
 import retrofit2.HttpException
 import java.util.concurrent.TimeUnit
@@ -58,47 +56,35 @@ class WeatherApiTest {
         server.shutdown()
     }
 
-    @Test
-    fun `get weather uses correct method and path when invoked`() = runTest {
-        val mockResponse = MockResponse().setBody(json)
-        server.enqueue(mockResponse)
-        val latitude = Latitude(34.149956)
-        val longitude = Longitude(-118.448891)
-        val apiKey = "apiKey"
-        val units = Units.Imperial.name.lowercase()
-        val expectedPath =
-            "/test/data/3.0/onecall" +
-                    "?lat=${latitude.value}" +
-                    "&lon=${longitude.value}" +
-                    "&appid=$apiKey" +
-                    "&units=$units"
-        val expectedMethod = "GET"
+    @Test()
+    fun when_get_weather_called_then_correct_method_and_path_used() =
+        runTest {
+            val mockResponse = MockResponse().setBody(json)
+            server.enqueue(mockResponse)
+            val latitude = Latitude(34.149956)
+            val longitude = Longitude(-118.448891)
+            val apiKey = "apiKey"
+            val units = Units.Imperial.name.lowercase()
+            val expectedPath =
+                "/test/data/3.0/onecall" +
+                        "?lat=${latitude.value}" +
+                        "&lon=${longitude.value}" +
+                        "&appid=$apiKey" +
+                        "&units=$units"
+            val expectedMethod = "GET"
 
-        weatherApi.getWeather(latitude, longitude, apiKey, units)
+            weatherApi.getWeather(latitude, longitude, apiKey, units)
 
-        val request = server.takeRequest()
-        val actualMethod = request.method
-        val actualPath = request.path
-        assertEquals(expectedMethod, actualMethod)
-        assertEquals(expectedPath, actualPath)
-    }
-
-    @Test
-    fun `get weather returns weather response when request is successful`() = runTest {
-        val mockResponse = MockResponse().setBody(json)
-        server.enqueue(mockResponse)
-
-        val actual = weatherApi.getWeather(Latitude(0.0), Longitude(0.0), "", "")
-
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun `get weather returns weather response with slow network`() = runTest {
-        val mockResponse = MockResponse().apply {
-            setBody(json)
-            throttleBody(1024, 1, TimeUnit.SECONDS);
+            val request = server.takeRequest()
+            val actualMethod = request.method
+            val actualPath = request.path
+            assertEquals(expectedMethod, actualMethod)
+            assertEquals(expectedPath, actualPath)
         }
+
+    @Test
+    fun when_get_weather_called_then_return_weather() = runTest {
+        val mockResponse = MockResponse().setBody(json)
         server.enqueue(mockResponse)
 
         val actual = weatherApi.getWeather(Latitude(0.0), Longitude(0.0), "", "")
@@ -107,7 +93,21 @@ class WeatherApiTest {
     }
 
     @Test
-    fun `get weather throws HttpException when server returns 400`() = runTest {
+    fun given_slow_network_when_get_weather_called_then_return_weather() =
+        runTest {
+            val mockResponse = MockResponse().apply {
+                setBody(json)
+                throttleBody(1024, 1, TimeUnit.SECONDS);
+            }
+            server.enqueue(mockResponse)
+
+            val actual = weatherApi.getWeather(Latitude(0.0), Longitude(0.0), "", "")
+
+            assertEquals(expected, actual)
+        }
+
+    @Test
+    fun given_400_when_get_weather_called_then_return_http_exception() = runTest {
         val expected = 400
         val mockResponse = MockResponse().setResponseCode(expected)
         server.enqueue(mockResponse)
@@ -120,7 +120,7 @@ class WeatherApiTest {
     }
 
     @Test
-    fun `get weather throws HttpException when server returns 500`() = runTest {
+    fun given_500_when_get_weather_called_then_return_http_exception() = runTest {
         val expected = 500
         val mockResponse = MockResponse().setResponseCode(expected)
         server.enqueue(mockResponse)
@@ -133,7 +133,7 @@ class WeatherApiTest {
     }
 
     @Test
-    fun `get weather throws ProtocolException when request is disconnected`() = runTest {
+    fun given_disconnection_when_get_weather_called_then_return_protocol_exception() = runTest {
         val byteArrayStub = ByteArray(1024)
         val mockResponse = MockResponse().apply {
             setBody(Buffer().write(byteArrayStub))
@@ -141,10 +141,8 @@ class WeatherApiTest {
         }
         server.enqueue(mockResponse)
 
-        assertThrows(ProtocolException::class.java) {
-            runBlocking {
-                weatherApi.getWeather(Latitude(0.0), Longitude(0.0), "", "")
-            }
+        assertFailsWith<ProtocolException> {
+            weatherApi.getWeather(Latitude(0.0), Longitude(0.0), "", "")
         }
     }
 }
