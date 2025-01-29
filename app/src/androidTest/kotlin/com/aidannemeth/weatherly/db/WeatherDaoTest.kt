8202738db0c9a206c105d3e9e49459a06f45fee9
iron_aidan <39450202+ironaidan@users.noreply.github.com>
2025-01-29 13:59:49 -0800

    Refactor WeatherDaoTest to use Turbine for Flow testing
    - Replaced `first()` with Turbine's `test()` for observing Flow emissions in WeatherDaoTest.
    - Introduced `awaitItem()` to assert on Flow values in a more controlled manner.
    - Added tests for state changes on inserts.
    - Added test to verify deletion behavior and count.
    - Removed unnecessary imports.
    - Renamed tests to better reflect testing scenario.

--- app/src/androidTest/kotlin/com/aidannemeth/weatherly/db/WeatherDaoTest.kt
+++ app/src/androidTest/kotlin/com/aidannemeth/weatherly/db/WeatherDaoTest.kt
@@ -1,18 +1,19 @@
 package com.aidannemeth.weatherly.db
 
+import app.cash.turbine.test
 import com.aidannemeth.weatherly.feature.weather.data.local.dao.WeatherDao
 import com.aidannemeth.weatherly.feature.weather.data.sample.WeatherEntitySample
 import com.aidannemeth.weatherly.feature.weather.domain.model.Temperature
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert.assertEquals
 import org.junit.Rule
 import javax.inject.Inject
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 @HiltAndroidTest
 class WeatherDaoTest {
@@ -38,33 +39,29 @@ class WeatherDaoTest {
     }
 
     @Test
-    fun `observe returns weather when existing in db`() = runTest {
-        val expected = weatherEntity
+    fun when_inserting_then_observe_updates() = runTest {
+        val updatedEntity = weatherEntity.copy(
+            temperature = Temperature(value = 100.0f)
+        )
 
-        weatherDao.insert(expected)
-        val actual = weatherDao.observe().first()
-
-        assertEquals(actual, expected)
+        weatherDao.observe().test {
+            assertNull(awaitItem())
+            weatherDao.insert(weatherEntity)
+            assertEquals(weatherEntity, awaitItem())
+            weatherDao.insert(updatedEntity)
+            assertEquals(updatedEntity, awaitItem())
+        }
     }
 
     @Test
-    fun `observe returns null when weather not existing in db`() = runTest {
-        val expected = null
+    fun given_db_contains_weather_when_deleting_then_count_is_zero() =
+        runTest {
+            val expected = 0
+            weatherDao.insert(weatherEntity)
 
-        val actual = weatherDao.observe().first()
+            weatherDao.deleteAll()
 
-        assertEquals(actual, expected)
-    }
-
-    @Test
-    fun `db only ever contains one row`() = runTest {
-        val expected = 1
-
-        weatherDao.insert(weatherEntity)
-        weatherDao.insert(weatherEntity.copy(temperature = Temperature(1.0f)))
-        weatherDao.insert(weatherEntity.copy(temperature = Temperature(2.0f)))
-        val actual = weatherDao.count()
-
-        assertEquals(actual, expected)
-    }
+            val actual = weatherDao.count()
+            assertEquals(expected, actual)
+        }
 }
